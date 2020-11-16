55252123d05adfb4d2cc1c7f8de5b33b438a4b7f
Alexandr Beidyk <65552941+alexbeidyk@users.noreply.github.com>
2020-11-16 15:06:22 +0200

    task 2 updated

--- js/task-2.js
+++ js/task-2.js
@@ -11,17 +11,9 @@ const ingredients = [
 
   const ingredientsRef = document.querySelector('#ingredients');
 
-  const li1 = document.createElement('li');
-  li1.textContent = ingredients[0];
-  const li2 = document.createElement('li');
-  li2.textContent = ingredients[1];
-  const li3 = document.createElement('li');
-  li3.textContent = ingredients[2];
-  const li4 = document.createElement('li');
-  li4.textContent = ingredients[3];
-  const li5 = document.createElement('li');
-  li5.textContent = ingredients[4];
-  const li6 = document.createElement('li');
-  li6.textContent = ingredients[5];
-
-ingredientsRef.append(li1, li2, li3, li4, li5, li6);
+const ingredientsArr = ingredients.map((item) => {
+  const ingredientsLi = document.createElement('li');
+    ingredientsLi.textContent = item;
+      ingredientsRef.append(ingredientsLi)
+        return ingredientsRef;
+});
